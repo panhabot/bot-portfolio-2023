b38ab21c4d9f764c13740f728a479b57dc580b5c
panhabot <kongpanhabot2000@gmail.com>
2023-09-12 16:16:12 +0900

    fix: mobile layout prevent scroll

--- src/Components/Page/MainPage.tsx
+++ src/Components/Page/MainPage.tsx
@@ -19,6 +19,26 @@ export const MainPage = () => {
     hidden: { opacity: 0, y: 10 },
     visible,
   };
+  const setFillHeight = () => {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  };
+
+  let vw = window.innerWidth;
+
+  window.addEventListener("resize", () => {
+    if (vw === window.innerWidth) {
+      // 画面の横幅にサイズ変動がないので処理を終える
+      return;
+    }
+
+    // 画面の横幅のサイズ変動があった時のみ高さを再計算する
+    vw = window.innerWidth;
+    setFillHeight();
+  });
+
+  // 初期化
+  setFillHeight();
   return (
     <motion.div
       initial="hidden"
