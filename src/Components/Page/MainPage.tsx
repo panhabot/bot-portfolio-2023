4666b909902f8917333f0f35c63f0a6bcae01838
panhabot <kongpanhabot2000@gmail.com>
2023-09-28 13:43:18 +0900

    chores: remove comment

--- src/Components/Page/MainPage.tsx
+++ src/Components/Page/MainPage.tsx
@@ -28,16 +28,13 @@ export const MainPage = () => {
 
   window.addEventListener("resize", () => {
     if (vw === window.innerWidth) {
-      // 画面の横幅にサイズ変動がないので処理を終える
       return;
     }
 
-    // 画面の横幅のサイズ変動があった時のみ高さを再計算する
     vw = window.innerWidth;
     setFillHeight();
   });
 
-  // 初期化
   setFillHeight();
   return (
     <motion.div
